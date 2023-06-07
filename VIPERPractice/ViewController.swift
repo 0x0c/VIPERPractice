6c5907dbd80a4d2150e5a65690d76f7d3d5a0037
Akira <akira.matsuda@me.com>
2023-06-07 21:30:20 +0900

    Initial Commit

---
+++ VIPERPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPERPractice
+//
+//  Created by Akira Matsuda on 2023/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
